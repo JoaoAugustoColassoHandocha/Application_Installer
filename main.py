f1279f9f351ff29c5a12518ab52d3d1fd3559cda
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-12 14:23:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ This file consists of creating the system menu.
 
 import os, defs
 
-os.system('color 1f') # Insertion of blue background color and white letters
+os.system('color 1f') # Insertion of blue background color and white letters.
 
 
 
-defs.pause()
-defs.clear()
+defs.pause() # Calling the 'pause' function to pause the code.
+defs.clear() # Calling the 'clear' function to clear the previous result of the code.
