a92e955ed8369e64cc3f461896fe12041299d91c
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-12 14:25:48 -0300

    Update defs.py

--- defs.py
+++ defs.py
@@ -5,16 +5,19 @@ This file consists of the creation of the system functions.
 
 import os
 
-os.system('color 1f') # Insertion of blue background color and white letters
+os.system('color 1f') # Insertion of blue background color and white letters.
 
+# Creating the 'clear' function to clear the previous result of the code.
 def clear():
     
     return os.system('cls')
 
+# Creating the 'pause' function, to pause the code.
 def pause():
     
     return os.system('pause')
 
-def main():
+# Creating the 'menu' function, for the system menu.
+def menu():
     
     ...
